368bebf44c75199140939a77b99138cf4a5e0eca
Eric Sirinian <e,v,sirinian@gmail.com>
2017-01-14 17:31:13 -0500

    Basic sign in layout complete

---
+++ SocialMedia/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SocialMedia
+//
+//  Created by Eric Sirinian on 1/14/17.
+//  Copyright © 2017 Eric Sirinian. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
+
+
+
+
