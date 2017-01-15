cc088745dbf3edfd0e66ae8d60c04e205ad7a11f
Eric Sirinian <e,v,sirinian@gmail.com>
2017-01-14 19:58:56 -0500

    Added FB authentification functionality

--- SocialMedia/SignInVC.swift
+++ SocialMedia/SignInVC.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +21,31 @@ class SignInVC: UIViewController {
         
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        let fbLoginBtn = FBSDKLoginManager()
+        
+        fbLoginBtn.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Unable to authenticate with FACEBOOK!")
+            } else if result?.isCancelled == true {
+                print("User cancelled FB Authenification")
+            } else {
+                print ("AUthenificated")
+                let credential = FIRGitHubAuthProvider.credential(withToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ crendential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: crendential, completion: { (user, error) in
+            if error != nil {
+                print("UNable to auth with Firbase - \(error)")
+            } else {
+                print("Success auth with FIREBASE")
+            }
+        })
+    }
 
 }
 
